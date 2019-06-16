c0c79779c5ea19c0a5d57f08a90e1913f9666429
laschenkov.artem <laschenkov.artem@otr.ru>
2019-06-16 12:49:15 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,96 @@
+import {
+  Component,
+  NgModule,
+  Input,
+  OnInit,
+  OnChanges,
+  ChangeDetectionStrategy
+} from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ApiService } from "./ApiService";
+
+@Component({
+  selector: "[header]",
+  template: `
+    <th>№</th>
+    <th>Name</th>
+    <th>Score</th>
+    <th>Note</th>
+  `
+})
+export class HeaderComponent {}
+
+@Component({
+  selector: "[item-detail]",
+  template: `
+    <td>{{ item.id }}</td>
+    <td>{{ item.name }}</td>
+    <td>{{ item.score }}</td>
+    <td><input size="7" (input)="saveValue($event)" [value]="note" /></td>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RowComponent implements OnChanges {
+  @Input("item-detail") item;
+
+  // my solution goes here ---
+  note = "";
+
+  ngOnChanges() {
+    this.note = this.isValueSet()
+      ? sessionStorage.getItem(this.itemName())
+      : "";
+  }
+
+  saveValue({ target }) {
+    this.note = target.value;
+    sessionStorage.setItem(this.itemName(), this.note);
+  }
+
+  isValueSet() {
+    return sessionStorage.getItem(this.itemName());
+  }
+
+  itemName() {
+    return `item-${this.item.id}`;
+  }
+  // my solution ends here ---
+}
+
+@Component({
+  selector: "app-root",
+  providers: [ApiService],
+  template: `
+    <table border="2" cellpadding="5">
+      <thead>
+        <tr header></tr>
+      </thead>
+      <tbody>
+        <tr *ngFor="let item of items; let i = index" [item-detail]="item"></tr>
+      </tbody>
+    </table>
+  `
+})
+export class AppComponent implements OnInit {
+  items;
+
+  constructor(private apiService: ApiService) {}
+  ngOnInit() {
+    this.apiService.handleChanges(changes => {
+      this.items = [...this.calcScore(changes)];
+    });
+  }
+  calcScore(items) {
+    return items.map(item => ({
+      ...item,
+      score: Math.round((item.rate / 100) * 5)
+    }));
+  }
+}
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent, RowComponent, HeaderComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
